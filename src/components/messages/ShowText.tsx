88d01bf258e524ff38f49838b1e6563fc10ec3b0
HannesCho <hoonsungcho11@gmail.com>
2023-02-05 14:48:56 +0100

    text change

--- src/components/messages/ShowText.tsx
+++ src/components/messages/ShowText.tsx
@@ -6,7 +6,7 @@ const ShowText = ({ cartValue, totalFee }: MessageProps) => {
       {cartValue ? (
         Number(cartValue) > 100 && (
           <p className="show-text" role="alert">
-            <span>**Your Cart Value is over € 100</span>
+            <span>Your Cart Value is over € 100</span>
             <span>Delivery Fee is free.</span>
           </p>
         )
@@ -16,7 +16,7 @@ const ShowText = ({ cartValue, totalFee }: MessageProps) => {
       {totalFee ? (
         totalFee >= 15 && (
           <p className="show-text" role="alert">
-            <span>**Total Delivery Fee is</span>
+            <span>Total Delivery Fee is</span>
             <span>no more than € 15.</span>
           </p>
         )
